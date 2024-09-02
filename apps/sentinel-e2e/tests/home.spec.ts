88f3616315176979a5e2aed5db6429f93e72034e
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-02 21:15:54 +0700

    update e2e test

---
+++ apps/sentinel-e2e/tests/home.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { BASE_URL } from "../config";
+
+test("Home page should display hello", async ({ page }) => {
+  // Navigate to the home page
+  await page.goto(BASE_URL);
+
+  // Check if the h1 element contains the text "hello"
+  const heading = await page.locator("h1");
+  await expect(heading).toHaveText("hello");
+
+  // Check if the h1 element has the correct styles
+  await expect(heading).toHaveClass(/text-4xl/);
+  await expect(heading).toHaveClass(/font-bold/);
+  await expect(heading).toHaveClass(/text-black/);
+});
