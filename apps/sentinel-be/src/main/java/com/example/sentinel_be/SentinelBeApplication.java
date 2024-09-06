ae07519d585aaff83e0a843bffb469fd2f79b542
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-06 16:58:31 +0700

    add sentinel-be project

---
+++ apps/sentinel-be/src/main/java/com/example/sentinel_be/SentinelBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentinel_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelBeApplication.class, args);
+	}
+
+}
---
+++ apps/sentinel-be/src/test/java/com/example/sentinel_be/SentinelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentinel_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
